5fa44a2b975c8f8fff63ac49e06ba8d595120ea6
José Victor <josevitorppessoa@gmail.com>
2022-07-11 13:46:19 -0300

    CRUD Finished - Deleting User

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -10,7 +10,8 @@
                         <th scope="col">Nome</th>
                         <th scope="col">Email</th>
                         <th scope="col">Data de cadastro</th>
-                        <th scope="col">Ações</th>
+                        <th scope="col">Visualizar</th>
+                        <th scope="col">Deletar</th>
                     </tr>
                 </thead>
                 <tbody class="text-center">
@@ -21,7 +22,15 @@
                         <td>{{ date('d/m/Y', strtotime($user->created_at)) }}</td>
                         <td>
                             <a href=" {{ route('users.edit', $user->id) }}" class="btn btn-warning text-white">Editar</a>
-                            <a href="" class="btn btn-danger text-white">Deletar</a>
+                        </td>
+
+                        <td>
+                            <form action="{{ route('users.destroy', $user->id) }}" method="post">
+                                @method('DELETE')
+                                @csrf
+                                <button class="btn btn-danger text-white">Deletar</button>
+
+                            </form>
                         </td>
                     </tr>
                 </tbody>
